06966be4f65393ae72711008ca074192b1de1c3b
ben <benf@ED-L-BENFISHMAN>
2017-06-03 20:27:28 +0300

    added ab

--- CppCatchDaFlagMTA/AbstractPlayer.h
+++ CppCatchDaFlagMTA/AbstractPlayer.h
@@ -1,4 +1,15 @@
 #pragma once
+
+#include <iostream>
+#include "BoardData.h"
+
+struct GameMove {
+	const int from_x, from_y;
+	const int to_x, to_y;
+	GameMove(int x1, int y1, int x2, int y2) : from_x(x1), from_y(y1), to_x(x2), to_y(y2) { }
+	;
+};
+
 class AbstractPlayer {
 public:
 	virtual ~AbstractPlayer() { }
@@ -6,6 +17,5 @@ public:
 	virtual void setPlayer(int player) = 0;
 	virtual void init(const BoardData& board) = 0;
 	virtual GameMove play(const GameMove& opponentsMove) = 0;
-	virtual string getName() const = 0;
-	;
-}
+	virtual std::string getName() const = 0;
+};
