27e2144a7e1ea1c090e7c3ef351daf0ffe20d63c
ben <benf@ED-L-BENFISHMAN>
2017-06-07 15:56:20 +0300

    r

--- CppCatchDaFlagMTA/AlgorithmPlayer.h
+++ CppCatchDaFlagMTA/AlgorithmPlayer.h
@@ -15,7 +15,7 @@ public:
 	virtual void init(const BoardData& board) {
 		for (int i = 0;i < board.rows;i++) {
 			for (int j = 0;j < board.cols;j++) {
-				const char c = board.charAt[i][j];
+				cout << board.charAt(i, j);
 			}
 			cout << " ! " << endl;
 		}
