e4ad4ba168c65cc826b1e5ff7a94682a3f42d0c0
ben <benf@ED-L-BENFISHMAN>
2017-06-10 14:29:01 +0300

    n

--- CppCatchDaFlagMTA/AlgorithmPlayer.h
+++ CppCatchDaFlagMTA/AlgorithmPlayer.h
@@ -13,7 +13,12 @@ public:
 	AlgorithmPlayer(string name) : Player(name) {};
 	virtual void setPlayer(int player) { playerKey = player; };
 	virtual void init(const BoardData& board) {
-		
+		for (int i = 0;i < 13;i++) {
+			for (int j = 0;j < 13;j++) {
+				cout << board.charAt(i, j);
+			}
+			cout << endl;
+		}
 	};
 	virtual GameMove play(const GameMove& opponentsMove) { return GameMove(0, 0, 0, 0); };
 
