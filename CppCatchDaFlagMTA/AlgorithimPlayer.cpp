b20dad02638baed0bff0e6634139cbe2beb385bf
Tomer Aben <tomerab@roydev.com>
2017-06-16 17:58:43 +0300

    Algo almost done

--- CppCatchDaFlagMTA/AlgorithimPlayer.cpp
+++ CppCatchDaFlagMTA/AlgorithimPlayer.cpp
@@ -1,12 +1,31 @@
 #include "stdafx.h"
+#include <map>
+#include <string>
 #include "AlgorithmPlayer.h"
 
 GameMove AlgorithmPlayer::play(const GameMove& opponentsMove) {
+	if (opponentsMove.from_x != opponentsMove.to_x ||
+		opponentsMove.from_y != opponentsMove.to_y)
+		changeBoard(opponentsMove);
 	
 	return GameMove(myTools[0].cords.x, myTools[0].cords.y, myTools[0].cords.x, myTools[0].cords.y + 1);
 
 }
 
+void AlgorithmPlayer::changeBoard(const GameMove& move) {
+	PlayerCoordinate nextMove{ move.to_x, move.to_y };
+
+	for (int i = 0; i < TOOLS_SIZE; ++i) {
+		if (oppTools[i] == nextMove)
+			oppTools[i].cords.set(nextMove.x, nextMove.y);
+	}
+
+	calcMoves();
+
+}
+void AlgorithmPlayer::getNextMove(AlgoTool* tool) {
+
+}
 void AlgorithmPlayer::init(const BoardData& board) {
 	char c;
 	for (int i = 0; i < board.cols; ++i) {
@@ -43,5 +62,134 @@ void AlgorithmPlayer::init(const BoardData& board) {
 			}
 		}
 	}
+	calcMoves();
 
 };
+
+void AlgorithmPlayer::calcMoves() {
+
+	for (int i = 0; i < TOOLS_SIZE; ++i) {
+		setToolBoards(&(myTools[i]));
+		calcOppWay(&(oppTools[i]));
+	}
+}
+
+void AlgorithmPlayer::calcOppWay(AlgoTool* tool) {
+	//if (tool->type == '#') {
+		tool->runCost = tool->cords.distance(MyFlag);
+	//}
+}
+void AlgorithmPlayer::setToolBoards(AlgoTool* tool) {
+	/* tool is the index 0-2 */
+
+	setToolBoardBlocks(tool);
+	setAnemiesOnToolsBoards(tool);
+	setLeePathOnBoard(tool);
+	// TODO: set killer cordinates for each tool
+	// setAnemiesOnToolsBoards(tool);
+	// TODO: make a Lee algo for fastest way and send the closest to catch the flag
+
+	cout << "";
+}
+
+void AlgorithmPlayer::setToolBoardBlocks(AlgoTool* tool) {
+	// set tool uneligible ways on path board
+	for (int i = 0; i < BoardData::cols; ++i) {
+		for (int j = 0; j < BoardData::rows; ++j) {
+			if (board[i][j] == FR)
+				tool->pathBoard[i][j] = (tool->elgLevel != 3) ? AlgoTool::BLOCKED_CELL : AlgoTool::EMPTY_CELL;
+			else if (board[i][j] == SEA)
+				tool->pathBoard[i][j] = !(tool->elgLevel == 3 || tool->elgLevel == 2) ? AlgoTool::BLOCKED_CELL : AlgoTool::EMPTY_CELL;
+		}
+	}
+}
+
+
+void AlgorithmPlayer::setLeePathOnBoard(AlgoTool* tool) {
+	int x = tool->cords.x, y = tool->cords.y;
+	tool->pathBoard[x][y] = 1;
+	setNextStep(tool, x - 1, y, 1);
+	setNextStep(tool, x + 1, y, 1);
+	setNextStep(tool, x, y - 1, 1);
+	setNextStep(tool, x, y + 1, 1);
+
+	tool->runCost = 0;
+	setToolMovesQueue(tool, targetFlag.x, targetFlag.y, '\0');
+	cout << "";
+}
+
+int AlgorithmPlayer::getPosAvilable(AlgoTool* tool, int x, int y, char pre_dir, char next_dir) {
+	return isCordOnBoard({ x, y }) && pre_dir != next_dir && tool->pathBoard[x][y] != AlgoTool::BLOCKED_CELL ? tool->pathBoard[x][y] : 99;
+}
+void AlgorithmPlayer::setToolMovesQueue(AlgoTool* tool, int x, int y, char dir) {
+	if (x == tool->cords.x && y == tool->cords.y)
+		return;
+	
+
+	tool->runCost += 1;
+
+	if (dir != '\0') tool->expectedMoves.push(dir);
+
+	int up = getPosAvilable(tool, x, y + 1, dir, 'u'),
+		down = getPosAvilable(tool, x, y - 1, dir, 'd') ,
+		right = getPosAvilable(tool, x - 1, y, dir, 'r'),
+		left = getPosAvilable(tool, x + 1, y, dir, 'l');
+
+	if (up < down && up < right && up < left && up != 99)
+		setToolMovesQueue(tool, x, y + 1, 'd');
+	else if (down < right && down < left && down != 99)
+		setToolMovesQueue(tool, x, y - 1, 'u');
+	else if (right < left && right != 99)
+		setToolMovesQueue(tool, x - 1, y, 'l');
+	else if (left != 99) setToolMovesQueue(tool, x + 1, y, 'r');
+
+}
+
+void AlgorithmPlayer::setNextStep(AlgoTool* tool, int x, int y, int preLevel) {
+	if (!isXYOnBoard(x, y) || tool->pathBoard[x][y] == AlgoTool::BLOCKED_CELL 
+		|| (tool->pathBoard[x][y] < preLevel && tool->pathBoard[x][y] != AlgoTool::EMPTY_CELL) 
+		|| (x == targetFlag.x && y == targetFlag.y))
+		return;
+	++preLevel;
+
+	tool->pathBoard[x][y] = preLevel;
+
+	setNextStep(tool, x - 1, y, preLevel);
+	setNextStep(tool, x + 1, y, preLevel);
+	setNextStep(tool, x, y - 1, preLevel);
+	setNextStep(tool, x, y + 1, preLevel);
+
+}
+
+void AlgorithmPlayer::setAnemiesOnToolsBoards(AlgoTool* tool) {
+	for (int i = 0; i < TOOLS_SIZE; ++i) {
+		setAnemyOnToolsBoard(tool, oppTools[i].cords.x, oppTools[i].cords.y);
+	}
+}
+
+void AlgorithmPlayer::setKillerWayToolsBoards(AlgoTool* tool) {
+}
+
+
+void AlgorithmPlayer::setAnemyOnToolsBoard(AlgoTool* tool, int x, int y) {
+	PlayerCoordinate* cords = anemyRounds(x, y);
+	for (int i = 0 ; i < 5; ++i)
+	{
+		if (isCordOnBoard(cords[i]))
+			tool->pathBoard[cords[i].x][cords[i].y] = AlgoTool::BLOCKED_CELL;
+	}
+}
+
+
+AlgorithmPlayer::PlayerCoordinate* AlgorithmPlayer::anemyRounds(int x, int y) {
+	PlayerCoordinate cords[5];
+
+	cords[0].set(x, y);
+	cords[1].set(x, y - 1);
+	cords[2].set(x, y + 1);
+	cords[3].set(x - 1, y);
+	cords[4].set(x + 1, y);
+
+	return cords;
+
+}
--- CppCatchDaFlagMTA/AlgorithmPlayer.h
+++ CppCatchDaFlagMTA/AlgorithmPlayer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <queue>
 #include "Player.h"
 #include "BoardData.h"
 
@@ -14,10 +15,22 @@ class AlgorithmPlayer : public Player {
 
 	struct PlayerCoordinate {
 		int x = -1, y = -1;
+		PlayerCoordinate(int _x, int _y) : x(_x), y(_y) {}
+		PlayerCoordinate() {}
+		void set(int _x, int _y) { 
+			x = _x; 
+			y = _y; 
+		}
+		bool operator==(PlayerCoordinate cord) { return cord.x == x && cord.y == y; }
+		int distance(int target_x, int target_y) {return abs(x - target_x) + abs(y - target_y);}
+		int distance(PlayerCoordinate cord) {return distance(cord.x, cord.y);}
 	};
-
 	struct AlgoTool {
 		PlayerCoordinate cords;
+		enum { BLOCKED_CELL = -10, EMPTY_CELL = 0 };
+		enum PRIORIY_CELL { KILLER_CELL = -4, TREES_ELGIBLE = -3, SEA_ELGIBLE = -2, KILL_OPT = -2};
+		int pathBoard[BoardData::cols][BoardData::rows]{ EMPTY_CELL };
+		queue <char> expectedMoves;
 		/*
 		 * elgLevel: 
 		 * 3 - elgible sea and forest
@@ -26,32 +39,59 @@ class AlgorithmPlayer : public Player {
 		 * 0 - Unkown
 		 */
 		int elgLevel = 0, runCost = -1;
+		bool alive = true;
 
 		char type = '#'; // Player type '#' is unkown
+		bool operator==(char type) { return this->type == type; }
+		bool operator==(PlayerCoordinate cord) { return this->cords == cord; }
+
 	};
 	PlayerCoordinate MyFlag, targetFlag;
 	AlgoTool myTools[TOOLS_SIZE];
 	AlgoTool oppTools[TOOLS_SIZE];
 
+	bool isToolAlive(AlgoTool* tool) {
+		return board[tool->cords.x][tool->cords.y] == tool->type;
+	}
 	void setMyTool(int index, int elg, char type, int x, int y) {
 		myTools[index].elgLevel = elg;
 		myTools[index].type = type;
-		myTools[index].cords.x = x;
-		myTools[index].cords.y = y;
+		myTools[index].cords = { x, y };
 	}
 
 	void setAnemy(int x, int y) {
 		int i = 0;
 		for (; oppTools[i].cords.x == -1; ++i);
-		oppTools[i].cords.x = x;
-		oppTools[i].cords.y = y;
+		oppTools[i].cords = { x, y };
 	}
+	bool isCordOnBoard(PlayerCoordinate pos) {
+		return isXYOnBoard(pos.x, pos.y);
+	}
+	bool isXYOnBoard(int x, int y) {
+		if (x < 0 || x >= BoardData::cols ||
+			y < 0 || y >= BoardData::rows)
+			return false;
+		return true;
+	}
+
+	void setToolBoards(AlgoTool* tool);
+	void calcOppWay(AlgoTool* tool);
+	void getNextMove(AlgoTool* tool);
+	void setNextStep(AlgoTool* tool, int x, int y, int level);
+	void setAnemiesOnToolsBoards(AlgoTool* tool);
+	void setAnemyOnToolsBoard(AlgoTool* tool, int x, int y);
+	void setKillerWayToolsBoards(AlgoTool* tool);
+	void setLeePathOnBoard(AlgoTool* tool);
+	void changeBoard(const GameMove& move);
+	void setToolBoardBlocks(AlgoTool* tool);
+	void calcMoves();
+	int getPosAvilable(AlgoTool* tool, int x, int y, char pre_dir, char next_dir);
+	void setToolMovesQueue(AlgoTool* tool, int x, int y, char dir);
+	PlayerCoordinate* anemyRounds(int x, int y);
 
 public:
 	AlgorithmPlayer(string name) : Player(name) {};
 	virtual void setPlayer(int player) { playerKey = player; };
 	virtual void init(const BoardData& board);
 	virtual GameMove play(const GameMove& opponentsMove);
-
-
 };
